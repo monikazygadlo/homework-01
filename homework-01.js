6342644157ba566844b66ccbaa9824538e8573da
monikazygadlo <moniczka.zygadlo@gmail.com>
2018-02-12 22:26:32 +0100

    working function(not tested yet)

--- homework-01.js
+++ homework-01.js
@@ -1,24 +1,38 @@
-function numbers(min,max,onlyOdd) {
+function numbers(min, max, onlyOdd) {
     let array = [];
-    if(onlyOdd=true){
-        if(min%2 == 0){ //pierwsza liczba parzysta zaczynamy od min+1
-            for (var i = min+1; i <= max; i++) {
+    if (min > max) {    //sprawdzenie czy min to min a max to max
+        let min = max;
+        let max = min;
+    }
+    if (onlyOdd == true) {
+        if (min % 2 == 0) { //pierwsza liczba parzysta zaczynamy od min+1
+            for (var i = min + 1; i <= max; i += 2) {
                 array.push(i);
             }
-            else{       //pierwsza liczba nieparzysta zaczynamy od min
-
+        }
+        else {       //pierwsza liczba nieparzysta zaczynamy od min
+            for (var i = min; i <= max; i += 2) {
+                array.push(i);
             }
         }
-        else { //onlyOdd jest fałsz generujemy tylko parzyste
-
+    }
+    else { //onlyOdd jest fałsz generujemy tylko parzyste
+        if (min % 2 == 0) { //pierwsza liczba parzysta zaczynamy od min+1
+            for (var i = min; i <= max; i += 2) {
+                array.push(i);
+            }
+        }
+        else {       //pierwsza liczba nieparzysta zaczynamy od min+1
+            for (var i = min + 1; i <= max; i += 2) {
+                array.push(i);
+            }
         }
     }
-    else{
-
-    }
+    console.log(array);
 }
 
-console.log(numbers(2,10,1))
+numbers(2, 10, 0);
+
 
 //min=1, max=10 10
 //min=1, max=11 11
