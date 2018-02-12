1af9c12ce680a6c1534e99f0c49a734a1825e513
monikazygadlo <moniczka.zygadlo@gmail.com>
2018-02-12 22:53:44 +0100

    min, max identification

--- homework-01.js
+++ homework-01.js
@@ -1,10 +1,8 @@
-function numbers(min, max, onlyOdd) {
+function numbers(minimum, maximum, onlyOdd) {
     let array = [];
-    if (min > max) {    //sprawdzenie czy min to min a max to max
-        let min = max;
-        let max = min;
-    }
-    if (onlyOdd == true) {
+    let min = Math.min(minimum, maximum);
+    let max = Math.max(minimum, maximum);
+    if (onlyOdd == true) { //generujemy tylko nieparzyste
         if (min % 2 == 0) { //pierwsza liczba parzysta zaczynamy od min+1
             for (var i = min + 1; i <= max; i += 2) {
                 array.push(i);
@@ -17,7 +15,7 @@ function numbers(min, max, onlyOdd) {
         }
     }
     else { //onlyOdd jest fałsz generujemy tylko parzyste
-        if (min % 2 == 0) { //pierwsza liczba parzysta zaczynamy od min+1
+        if (min % 2 == 0) { //pierwsza parzysta zaczynamy od min
             for (var i = min; i <= max; i += 2) {
                 array.push(i);
             }
@@ -28,10 +26,11 @@ function numbers(min, max, onlyOdd) {
             }
         }
     }
-    console.log(array);
+    console.log(array); //potrzebne do testów
 }
 
-numbers(2, 10, 0);
+numbers(10, 2, 0);
+
 
 
 //min=1, max=10 10
