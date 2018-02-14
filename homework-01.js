1ef6085507b04ce4fdd38c7f54863e3a988699dd
monikazygadlo <moniczka.zygadlo@gmail.com>
2018-02-14 22:45:20 +0100

    all 3 issues solved

--- homework-01.js
+++ homework-01.js
@@ -2,39 +2,44 @@ function numbers(minimum, maximum, onlyOdd) {
     let array = [];
     let min = Math.min(minimum, maximum);
     let max = Math.max(minimum, maximum);
-    if (onlyOdd == null) {
+    if (typeof (onlyOdd) === "undefined") {
         for (var i = min; i <= max; i++) {
             array.push(i);
+            return array;
+        }
+    } else {
+        if (onlyOdd === true) { //generujemy tylko nieparzyste
+            if (min % 2 === 0) { //pierwsza liczba parzysta zaczynamy od min+1
+                for (var i = min + 1; i <= max; i += 2) {
+                    array.push(i);
+                    return array;
+                }
+            }
+            else {       //pierwsza liczba nieparzysta zaczynamy od min
+                for (var i = min; i <= max; i += 2) {
+                    array.push(i);
+                    return array;
+                }
+            }
+        }
+        else if (onlyOdd === false) { //onlyOdd jest fałsz generujemy tylko parzyste
+            if (min % 2 === 0) { //pierwsza parzysta zaczynamy od min
+                for (var i = min; i <= max; i += 2) {
+                    array.push(i);
+                    return array;
+                }
+            }
+            else {       //pierwsza liczba nieparzysta zaczynamy od min+1
+                for (var i = min + 1; i <= max; i += 2) {
+                    array.push(i);
+                    return array;
+                }
+            }
         }
     }
-    if (onlyOdd == true) { //generujemy tylko nieparzyste
-        if (min % 2 == 0) { //pierwsza liczba parzysta zaczynamy od min+1
-            for (var i = min + 1; i <= max; i += 2) {
-                array.push(i);
-            }
-        }
-        else {       //pierwsza liczba nieparzysta zaczynamy od min
-            for (var i = min; i <= max; i += 2) {
-                array.push(i);
-            }
-        }
-    }
-    else { //onlyOdd jest fałsz generujemy tylko parzyste
-        if (min % 2 == 0) { //pierwsza parzysta zaczynamy od min
-            for (var i = min; i <= max; i += 2) {
-                array.push(i);
-            }
-        }
-        else {       //pierwsza liczba nieparzysta zaczynamy od min+1
-            for (var i = min + 1; i <= max; i += 2) {
-                array.push(i);
-            }
-        }
-    }
-    console.log(array); //potrzebne do testów
 }
 
-numbers(10, 2, 1);
+numbers(10, 2);
 
 
 //min=1, max=10 10
