6496ec40d6e05740ec8436c5a0a0a7f3681f74cb
monikazygadlo <moniczka.zygadlo@gmail.com>
2018-02-20 09:16:40 +0100

    comments in english testing variuos scenarios

--- homework-01.js
+++ homework-01.js
@@ -1,48 +1,49 @@
 function numbers(minimum, maximum, onlyOdd) {
-    let array = [];
-    let min = Math.min(minimum, maximum);
-    let max = Math.max(minimum, maximum);
+    var array = [];
+    var min = Math.min(minimum, maximum);
+    var max = Math.max(minimum, maximum);
     if (typeof (onlyOdd) === "undefined") {
         for (var i = min; i <= max; i++) {
             array.push(i);
-            return array;
         }
     } else {
-        if (onlyOdd === true || onlyOdd === 1) { //generujemy tylko nieparzyste
-            if (min % 2 === 0) { //pierwsza liczba parzysta zaczynamy od min+1
-                for (var i = min + 1; i <= max; i += 2) {
-                    array.push(i);
-                    return array;
-                }
-            }
-            else {       //pierwsza liczba nieparzysta zaczynamy od min
+        if (onlyOdd === true || onlyOdd === 1) { //only odd numbers
+            if (min % 2 !== 0) {       //min is odd so start from min
                 for (var i = min; i <= max; i += 2) {
                     array.push(i);
-                    return array;
+                }
+            }
+            else { //min is even so start from min+1
+                for (i = min + 1; i <= max; i += 2) {
+                    array.push(i);
                 }
             }
         }
-        else if (onlyOdd === false || onlyOdd === 0) { //onlyOdd jest fałsz generujemy tylko parzyste
-            if (min % 2 === 0) { //pierwsza parzysta zaczynamy od min
+        else if (onlyOdd === false || onlyOdd === 0) { //only even number
+            if (min % 2 === 0) { //min is even so start from min
                 for (var i = min; i <= max; i += 2) {
                     array.push(i);
-                    return array;
                 }
             }
-            else {       //pierwsza liczba nieparzysta zaczynamy od min+1
+            else {       //min is odd do start from min+1
                 for (var i = min + 1; i <= max; i += 2) {
                     array.push(i);
-                    return array;
                 }
             }
         }
     }
+    return array;
 }
+console.log(numbers(3, 10, true));
+console.log(numbers(3, 10, 1));
+console.log(numbers(3, 10, false));
+console.log(numbers(3, 10, 0));
+console.log(numbers(3, 10));
+console.log(numbers(11, 1, true));
+console.log(numbers(11, 1, 1));
+console.log(numbers(11, 1, false));
+console.log(numbers(11, 1, 0));
+console.log(numbers(11, 1));
+console.log(numbers(11, 11, 0));
+console.log(numbers(11, 11, 1));
 
-numbers(3, 10, 0);
-
-
-//min=1, max=10 10
-//min=1, max=11 11
-//min=2, max=10
-//min=2, max=11
